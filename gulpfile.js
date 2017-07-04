6bce47d919ae19185e944b875fa53613dca03187
Konstiantyn Yukhariev <yukharev@gmail.com>
2017-07-04 08:57:46 +0300

    Added gulp, setup sass, other changes.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      cleanCSS = require('gulp-clean-css');
+
+gulp.task('css', () => {
+  return gulp.src('./wufoo.scss')
+    .pipe(sass()
+      .on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./*.scss', ['css']);
+});
+
+gulp.task('default', ['watch', 'css']);
